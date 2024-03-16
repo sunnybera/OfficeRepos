99c6c2104704f4dae5bf087464cce3cca04ea35d
Sunny Bera <sunny.bera@yahoo.co.in>
2024-03-16 12:49:42 +0530

    Azure IaC

---
+++ iotreewebapp-lab/IOTreeWebApplication/IOTreeWebForm_1.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="IOTreeWebForm_1.aspx.cs" Inherits="IOTreeWebApplication.IOTreeWebForm_1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        Type Your Full Name Here<br />
+        <br />
+        <asp:TextBox ID="TextBox1" runat="server" Width="276px"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Submit" />
+    </form>
+</body>
+</html>
---
+++ iotreewebapp-lab/IOTreeWebApplication/IOTreeWebForm_1.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace IOTreeWebApplication
+{
+    public partial class IOTreeWebForm_1 : System.Web.UI.Page
+    {
+        private string Label1;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack == true)
+            {
+                Label1 = ("Great job! Data was inserted.");
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection iotreeconn = new SqlConnection("Server=tcp:np-sql-srv-001.database.windows.net,1433;Initial Catalog=np-dls-db-001;Persist Security Info=False;User ID=calib-admin;Password=Welcome@555;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            {
+                SqlCommand insert = new SqlCommand("EXEC dbo.insertfullname @fullname", iotreeconn);
+                insert.Parameters.AddWithValue("@fullname", TextBox1.Text);
+
+                iotreeconn.Open();
+                insert.ExecuteNonQuery();
+                iotreeconn.Close();
+
+                if (IsPostBack)
+                {
+                    TextBox1.Text = "";
+                }
+            }
+        }
+    }
+}
