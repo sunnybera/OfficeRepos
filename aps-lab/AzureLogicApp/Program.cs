66693fbd7b6b5a549b482e781542ae21c0ed4a06
Sunny Bera <sunny.bera@yahoo.co.in>
2024-03-16 12:16:35 +0530

    adding aps-lab to git repo

---
+++ aps-lab/AzureLogicApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureLogicApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
